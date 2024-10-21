f14a2ecf6db7500ce187e4fa6da95fcdac933d54
hyunjin park <hulkcodeer@gmail.com>
2024-10-21 16:25:57 +0900

    :construction: admob thinkingBanner 추가

--- lib/utils/ad_helper.dart
+++ lib/utils/ad_helper.dart
@@ -19,11 +19,11 @@ class AdHelper {
   static String get thinkingLogBannerAdUnitId {
     if (Platform.isAndroid) {
       return kReleaseMode
-          ? 'ca-app-pub-3926120372825354/9831103698'
+          ? 'ca-app-pub-3926120372825354/3206743767'
           : 'ca-app-pub-3940256099942544/6300978111';
     } else if (Platform.isIOS) {
       return kReleaseMode
-          ? 'ca-app-pub-3926120372825354/4553095977'
+          ? 'ca-app-pub-3926120372825354/9575042699'
           : 'ca-app-pub-3940256099942544/2934735716';
     } else {
       throw UnsupportedError('Unsupported platform');
